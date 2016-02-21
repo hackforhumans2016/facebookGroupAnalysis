4450dfbb0fc3ad0a39459b9e0b1c77d8a824dda9
Matthias Orlikowski <mazsvr@gmail.com>
2016-02-21 07:00:14 -0500

    Viz loads multiple data sets

--- viz/app/scripts/controllers/main.js
+++ viz/app/scripts/controllers/main.js
@@ -12,30 +12,57 @@ angular.module('vizApp')
 
     var allData = [];
 
+    $scope.data = [];
+
+    $scope.datasets = [{
+      id: 0,
+      label: 'Syria Home Berlin',
+      path: 'data/syrianHomeBerlin_100.json'
+    }, {
+      id: 1,
+      label: 'Syrien in Deutschland',
+      path: 'data/syrienindeutschland_100.json'
+    }, {
+      id: 2,
+      label: 'Deutschland in Arabisch',
+      path: 'data/deutschlandInArabisch_100.json'
+    }];
+
+    $scope.selectedDataset = $scope.datasets[0];
+
     $scope.slider = {
-      min: 0,
-      max: 0.1,
+      min: 0.3,
+      max: 0.9,
       options: {
         floor: 0,
-        ceil: 32,
-        precision: 2,
+        ceil: 1,
+        precision: 3,
         step: 0.001
       }
     };
 
     var filterMinMaxSlider = function(object) {
-      return object.score <  $scope.slider.max && object.score > $scope.slider.min;
+      return object.score < $scope.slider.max && object.score > $scope.slider.min;
     };
 
-    dataService.getData().then(function(data) {
-      allData = data;
-      $scope.data = allData.filter(filterMinMaxSlider);
-      $log.log('Scope data length: ', $scope.data.length);
-    });
+    $scope.getData = function(path) {
+      $log.log(path);
+      dataService.getData(path).then(function(data) {
+        allData = data;
+        $log.log($scope.data);
+        var filteredData = allData.filter(filterMinMaxSlider);
+        $scope.data = filteredData;
+        $log.log($scope.data);
+      }, function(error) {
+        $log.error(error);
+      });
+    };
+
+    $scope.getData($scope.selectedDataset.path);
 
     $scope.$on("slideEnded", function() {
       $log.log('slide ended');
-      if(allData.length > 0) {
+      if (allData.length > 0) {
         $scope.$apply(function() {
           $scope.data = allData.filter(filterMinMaxSlider);
           $log.log('Scope data length: ', $scope.data.length);
--- viz/app/scripts/services/dataservice.js
+++ viz/app/scripts/services/dataservice.js
@@ -11,30 +11,42 @@ angular.module('vizApp')
   .factory('dataService', function($http, $q, $log, colWidth) {
 
     return {
-      getData: function() {
+      getData: function(path) {
         var deferred = $q.defer();
-        $http.get('data/content_100.json').success(function(data) {
+        $http.get(path).success(function(data) {
           data = data.map(function(elem) {
             var baseSize = colWidth;
 
             //secret sauce
-            var resizeFactor = Math.ceil(Math.log(elem.score+0.001)/2+4);
+            var resizeFactor = Math.ceil(Math.log(elem.score + 0.001) / 2 + 4);
 
             elem.style = {
               width: resizeFactor * baseSize,
               height: resizeFactor * baseSize
             };
 
+            elem.size = resizeFactor;
+
             if (elem.full_picture_x < elem.full_picture_y) {
-              elem.img = {style: {width: elem.style.width}};
+              elem.img = {
+                style: {
+                  width: elem.style.width
+                }
+              };
             } else {
-              elem.img = {style: {height: elem.style.height}};
+              elem.img = {
+                style: {
+                  height: elem.style.height
+                }
+              };
             }
 
             return elem;
           });
 
           deferred.resolve(data);
+        }).error(function(error) {
+          deferred.reject(error);
         });
         return deferred.promise;
       }
